a76f9957a000bf33b86bfdec77a5793add7265cd
gelintonx <gelintonx@gelintonxs-iMac-Pro.local>
2020-11-27 15:55:26 +0100

    add image field on product model

--- ShopBackend/products/models.py
+++ ShopBackend/products/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Product(models.Model):
 
     name = models.CharField(max_length=100)
+    image = models.ImageField()
     price = models.FloatField()
     category = models.CharField(max_length=100)
     description = models.TextField()
